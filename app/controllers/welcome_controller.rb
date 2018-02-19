3cb8e9e5c51ef08b753670ab91778b75ab71bc0d
kudubaba <kudubaba@gmail.com>
2018-02-19 17:32:57 -0500

    :q initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+    puts "Hey in Index"
+    puts "Hey in Index"
+    puts "Hey in Index"
+    puts "Hey in Index"
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
